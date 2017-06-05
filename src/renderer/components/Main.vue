85edcacfeae674b1ab3e2b2371afcc12657d073f
Olajide Afeez <jide4real43@gmail.com>
2017-06-05 01:58:27 +0100

    add beep at half time

--- src/renderer/components/Main.vue
+++ src/renderer/components/Main.vue
@@ -8,6 +8,12 @@
             <h2 class="team-name">
              {{ team }}   
             </h2>
+
+            <div class="title">
+                <h3 v-if='mid == 0'>Presentation</h3>
+                <h3 class="q-a" v-if='mid == 1'>Question & Answer</h3>
+                <h3 class="q-t" v-if='mid == -1'>Time Up !!!</h3>
+            </div>
             <div class="clock"></div>
 
             <div class="form">
@@ -22,7 +28,7 @@
 
                   <md-input-container>
                     <label>Minutes</label>
-                    <md-input type="number" v-model="time" @keyup.enter='start'></md-input>
+                    <md-input type="number" v-model="time" @keyup.enter='start' min='0'></md-input>
                 </md-input-container>
 
 
@@ -30,14 +36,27 @@
                     <md-button type='submit' class="md-raised md-primary">Start</md-button>
                     <md-button class="md-raised md-warn" v-on:click.native="stopTimer">Stop</md-button>
                 </div>
-
                 </form>
             </div>
+
+            <footer>
+                <p class="md-warn">Powered By SCOOLNETWORK</p>
+            </footer>
         </div>
     </div>
 </template>
 
 <style>
+    footer {
+        position: fixed;
+        bottom: 1px;
+        right: 40%;
+
+        text-transform: uppercase;
+        color: #00B8D4;
+        font-weight: bold;
+        font-size: 2em;
+    }
     .text-center {
         text-align: center;
     }
@@ -50,9 +69,26 @@
         position: fixed;
     }
     .team-name {
-        text-align: center;
         font-size: 4em;
-        margin-left: -6%;
+        position: absolute;
+        left: 37%;
+        top: 16%;
+    }
+    .title {
+        position: absolute;
+        font-size: 1.5em;
+        left: 42%;
+        top: 28%;
+    }
+    .title h3 {
+        color: #4CAF50;
+    }
+    .title .q-a {
+        margin-left: -22%;
+        color: #FDD835;
+    }
+    .title .q-t { /*time up*/
+        color: #F44336;
     }
     .form {
         position: fixed;
@@ -74,6 +110,13 @@
                 callbacks: {
 		        	interval() {
 		        		let time = this.factory.getTime().time;
+
+                        let mid = self.time * 60 / 2;
+
+                        if(mid == time) {
+                            self.beep.play();
+                            self.middle();
+                        }
 		        		
 		        		if(time == 3) {
                             let i = 0;
@@ -84,6 +127,7 @@
 			        		    self.buzz.play();
                                 i++;
                             }, 900);
+                            self.timeup();
 			        	}
 		        	}
 		        }
@@ -92,13 +136,21 @@
         },
         methods: {
             start() {
+                this.clock.setTime(this.time * 60);
                 this.clock.start();
                 this.stop = false;
+                this.mid = 0;
             },
             stopTimer() {            
                 this.stop = true;
                 this.clock.stop();
                 this.buzz.pause();
+            },
+            middle() {
+                this.mid = 1;
+            },
+            timeup() {
+                this.mid = -1;
             }
         },
         data() {
@@ -107,12 +159,16 @@
                 clock: {},
                 team: 'EPEX1730',
                 buzz: new Audio('static/buzz.mp3'),
-                stop: true
+                beep: new Audio('static/beep.mp3'),
+                stop: true,
+                mid: 0
             }
         },
         watch: {
             time(val) {
                 this.clock.stop();
+                if(val < 0)
+                    return;
                 this.clock.setTime(val * 60);
             }
         }
